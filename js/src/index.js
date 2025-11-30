715e209e9046eaeada6e1869070f5b75d99679b1
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-11-30 16:34:12 -0500

    initialize

--- js/src/index.js
+++ js/src/index.js
@@ -1,17 +1,20 @@
 import "../style/index.css";
-
-async function activate(app) {
-  // eslint-disable-next-line no-console
-  console.log("JupyterLab extension jupyterlab-singleton-consumer is activated!");
-}
+import {ISingletonProvider} from "jupyterlab-singleton-provider";
 
 const extension = {
-  activate,
+  activate: () => {
+    // eslint-disable-next-line no-console
+    console.log("JupyterLab extension jupyterlab-singleton-consumer is activated!");
+
+    // Check the exposed webcomponent
+    if (document.createElement("singleton-element").constructor === HTMLElement) {
+      console.error("jupyterlab-singleton-consumer: singleton-element is not defined");
+    }
+  },
   autoStart: true,
   id: "jupyterlab-singleton-consumer",
   optional: [],
-  requires: [],
+  requires: [ISingletonProvider],
 };
 
 export default extension;
-export {activate as _activate};
